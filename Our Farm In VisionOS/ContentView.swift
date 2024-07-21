177ca0fc022da3d731f599fc9810b29b95242239
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2024-07-21 13:09:37 +0800

    Initial Commit

---
+++ Our Farm In VisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Our Farm In VisionOS
+//
+//  Created by Hananiah on 2024/7/21.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Our Farm In VisionOS/Our_Farm_In_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Our_Farm_In_VisionOSApp.swift
+//  Our Farm In VisionOS
+//
+//  Created by Hananiah on 2024/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Our_Farm_In_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
